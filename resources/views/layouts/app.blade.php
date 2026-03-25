e8e1394d2d81237e10f31243bca345fc86118da0
ramadhanprinada <ramadhanpmuhammad@gmail.com>
2026-03-25 16:16:26 +0700

    add dropdown profile & change font family

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,14 @@
 
     <script src="https://unpkg.com/@popperjs/core@2"></script>
     <script src="https://unpkg.com/tippy.js@6"></script>
-
     <script defer src="https://unpkg.com/@alpinejs/persist@3.x.x/dist/cdn.min.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.15.8/dist/cdn.min.js"></script>
 
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Lexend:wght@100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -20,7 +20,7 @@
 
 
     {{-- Right Content --}}
-    <div class="flex items-center gap-4">
+    <div class="flex items-center gap-4"">
 
         {{-- Notification --}}
         <button class="relative flex items-center justify-center cursor-pointer w-10 h-10 bg-cyan-100 rounded-full hover:bg-cyan-200 transition">
@@ -32,11 +32,26 @@
         </button>
 
         {{-- User Profile --}}
-        <img 
-            src="{{ asset('icons/icon-user-profile.png') }}" 
-            alt="Foto Profile" 
-            class="w-12 h-12 rounded-full object-cover cursor-pointer"
-        >
+        <div class="relative" x-data="{ dropdownOpen: false }">
+
+            <img 
+                src="{{ asset('icons/icon-user-profile.png') }}" 
+                class="w-12 h-12 rounded-full object-cover cursor-pointer"
+                @click="dropdownOpen = !dropdownOpen"
+            >
+
+            <div 
+                x-show="dropdownOpen"
+                x-transition
+                @click.outside="dropdownOpen = false"
+                class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50"
+            >
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Profil</a>
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Pengaturan</a>
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Keluar</a>
+            </div>
+
+        </div>
 
     </div>
 
